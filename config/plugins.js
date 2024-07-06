34749754f541ca2ac52a46fba9180508c16038f2
Vinicius Felipe <viniciusfelipe.xd@gmail.com>
2024-07-06 17:10:34 -0300

    feature: add gcp provider in plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,20 @@
-module.exports = () => ({});
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") === "production") {
+    return {
+      upload: {
+        config: {
+          provider:
+            "@strapi-community/strapi-provider-upload-google-cloud-storage",
+          providerOptions: {
+            bucketName: env("BUCKET_NAME"),
+            publicFiles: true,
+            uniform: false,
+            basePath: "",
+          },
+        },
+      },
+    };
+  }
+
+  return {};
+};
